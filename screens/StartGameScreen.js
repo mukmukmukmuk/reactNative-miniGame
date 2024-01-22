258a5cfc5718faf7ce129e0bebf9c28e19b69301
mukmukmukmuk <ksm010825@naver.com>
2024-01-22 17:00:37 +0900

    4-51

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,27 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "./../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-      Button
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 24,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
