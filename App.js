528fbfd17b4f6f768efe1ad203acb804a438cc59
mukmukmukmuk <ksm010825@naver.com>
2024-02-07 16:46:06 +0900

    74

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import AppLoading from "expo-app-loading";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
   const pickedNumberHandler = (pickedNumber) => {
     setUserNumber(pickedNumber);
     setGameIsOver(false);
@@ -22,6 +23,10 @@ export default function App() {
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
+  const startNewGameHandler = () => {
+    setUserNumber(null);
+    setGuessRounds(0);
+  };
   if (!fontsLoaded) {
     return <AppLoading />;
   }
@@ -31,7 +36,14 @@ export default function App() {
     screen = (
       <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
     );
-  if (gameIsOver && userNumber) screen = <GameOverScreen />;
+  if (gameIsOver && userNumber)
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
   return (
     <LinearGradient
       colors={[Colors.primary700, Colors.accent500]}
